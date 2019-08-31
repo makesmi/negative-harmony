800cdcf33920591dacf0b7c515dfa26f157c6545
Markus Mustonen <makesmi@hotmail.com>
2019-08-31 22:06:38 +0300

    feature: user can write negative chords and positive chords will update accordingly

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, useRef, CSSProperties } from 'react'
+import React, { useState, ChangeEvent, useRef } from 'react'
 import Piano from './Piano'
 import Staff, { Chords } from './Staff'
 import { bestKey } from './StaffFunctions'
@@ -10,11 +10,12 @@ const App: React.FC = () => {
   const [key, setKey] = useState<number>(0)
   const [negativeKey, setNegativeKey] = useState<number>(0)
   const [selected, setSelected] = useState<number>(-1)
+  const [selectedNegative, setSelectedNegative] = useState<number>(-1)
   const [chords, setChords] = useState<Chords>({})
+  const [negativeChords, setNegativeChords] = useState<Chords>({})
   const [chordText, setChordText] = useState<string>('')
   const input = useRef<HTMLInputElement>(null)
   const negativeNotes = negativeMelody(notes, key, negativeKey)
-  const negativeChords = computeNegativeChords(chords, key, negativeKey)
 
   const pressKey = (key: number) => {
     const note = key - 3    //first key in the piano is A
@@ -26,15 +27,31 @@ const App: React.FC = () => {
   }
 
   const updateChordText = (event:ChangeEvent<HTMLInputElement>) => {
-    if(selected === -1 || notes[selected] === undefined){ return }
     const text = event.target.value.substring(0, 8)
     setChordText(text)
-    setChords({...chords, [selected]: text})
+    if(selected !== -1 && notes[selected] !== undefined){
+      setChords({...chords, [selected]: text})
+      const negative = negativeChord(text, key, negativeKey)
+      setNegativeChords({...negativeChords, [selected]: negative})
+    }else if(selectedNegative !== -1 && notes[selectedNegative] !== undefined){
+      setNegativeChords({...negativeChords, [selectedNegative]: text})
+      const positive = negativeChord(text, negativeKey, key)
+      setChords({...chords, [selectedNegative]: positive})
+    }
   }
 
   const selectNote = (index:number) => {
     setSelected(index)
+    setSelectedNegative(-1)
     setChordText(chords[index] || '')
+    if(!input.current){ return }
+    input.current.focus()
+  }
+
+  const selectNegative = (index:number) => {
+    setSelectedNegative(index)
+    setSelected(-1)
+    setChordText(negativeChords[index] || '')
     if(!input.current){ return }
     input.current.focus()
   }
@@ -62,8 +79,8 @@ const App: React.FC = () => {
       </div>
       <div>
         <h2>Negative Harmony:</h2>
-        <Staff notes={negativeNotes} chords={negativeChords} keyTone={negativeKey}
-            height={120} notesMax={25} />
+        <Staff notes={negativeNotes} chords={negativeChords} selected={selectedNegative} setSelected={selectNegative}
+            keyTone={negativeKey} height={120} notesMax={25} />
       </div>
       <div>
         <button style={buttonStyle} onClick={keyIncrement(-1)}>-</button>
@@ -73,12 +90,5 @@ const App: React.FC = () => {
   )
 }
 
-const computeNegativeChords = (chords: Chords, fromKey: number, toKey: number) => {
-  const result: Chords = {}
-  for(let i in chords){
-    result[i] = negativeChord(chords[i], fromKey, toKey)
-  }
-  return result
-}
 
 export default App
