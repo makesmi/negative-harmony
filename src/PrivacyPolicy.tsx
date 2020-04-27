0c4017835e4a920989bbb45c862e1ecca73b1414
Markus Mustonen <makesmi@hotmail.com>
2020-04-27 21:10:01 +0300

    addition: privacy policy for Facebook

---
+++ src/PrivacyPolicy.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PrivacyPolicy:React.FC = () => (
+  <p>
+    Negative Harmony application does not process or collect any personal data. 
+    Only Facebook user ID is used so that users can log in into Negative Harmony application.
+    Songs will be saved into database with that id.
+    Other users cannot see songs or Facebook user ID.
+    Connection is protected with TLS. 
+  </p>
+)
+
+export default PrivacyPolicy
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { configure } from './Environment'
- 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
+import PrivacyPolicy from './PrivacyPolicy'
+
 configure()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = (
+  <Router>
+    <Switch>
+      <Route path="/privacy">
+        <PrivacyPolicy />
+      </Route>
+      <Route path="/">
+        <App />
+      </Route>
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
